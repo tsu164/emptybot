49e3bac4dd9290909c0ff2a9f6eacd5f6dad237f
tsu164 <tsu.knsk@gmail.com>
2020-09-16 02:36:19 +0900

    応答とリスト追加

--- discordbot.py
+++ discordbot.py
@@ -7,10 +7,33 @@ token = os.environ['DISCORD_BOT_TOKEN']
 
 
 @bot.event
+async def on_ready():
+    print('on_ready')
+
+@bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
+
+@bot.event
+async def on_message(message):
+    #bot相手なら無視
+    if message.author == bot.user:
+        return
+    if message.content == 'ローソン':
+        await message.channel.send('行け')
+    if message.content == 'からあげクン':
+        aawait message channel.send('食え')
+    poops = ['うんこ', 'うんち' , 'ウンコ' , 'ウンチ', '糞']
+    for poop in poops:
+        if poop in message.content:
+            await message.add_reaction(':poop:')
+
+@bot.event
+async def on_guild_join(guild):
+    await after.edit(nick = 'はみるとん')
+
 
 
 @bot.command()
