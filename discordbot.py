e64744119c1cea498a861732c011fc0bf1a8eeb0
tsu164 <tsu.knsk@gmail.com>
2020-10-13 05:29:52 +0900

    error文追加

--- discordbot.py
+++ discordbot.py
@@ -1,12 +1,15 @@
 from discord.ext import commands
 import os
+import traceback
 
 bot = commands.Bot(command_prefix='$')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 @bot.event
 async def on_command_error(ctx, error):
-    await ctx.send("なんか変だよ")
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
 
 @bot.event
 async def on_message(message):
