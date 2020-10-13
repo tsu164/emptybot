d24e966ffae472f3af0edccd8d4ad0fb1a4f2319
tsu164 <tsu.knsk@gmail.com>
2020-10-13 13:04:13 +0900

    出力結果のフォーマットとエラー文修正

--- cogs/gacha.py
+++ cogs/gacha.py
@@ -1,6 +1,8 @@
 from discord.ext import commands
 import random
 
+gacha_list = ["alcohol", "drink", "food", "konbini", "misosoup", "shop", "sweets", "tsumami"]
+
 class GachaSystem(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -10,12 +12,16 @@ class GachaSystem(commands.Cog):
         """ガチャができます
         alcohol, drink, food, konbini, misosoup, shop, sweets, tsumami
         数字でいくつか指定できます"""
-        with open(f"/app/list/{category}.txt") as f:
-            category_contents = [s.strip() for s in f.readlines()]
+        try:
+            with open(f"/app/list/{category}.txt") as f:
+                category_contents = [s.strip() for s in f.readlines()]
         if count == 0 or count > 10:
             return await ctx.send("ムチャ言うな")
-        for i in range(count):
-            await ctx.send(random.choice(category_contents))
+        await ctx.send(" ".join(random.sample(category_contents, count)))
+
+        except FileNotFoundError:
+            return await ctx.send("そんなガチャないよ")
+        
 
     @gacha.error
     async def gacha_error(self, ctx, error):
@@ -23,6 +29,18 @@ class GachaSystem(commands.Cog):
             await ctx.send("数字で言って！")
         if isinstance(error, commands.errors.MissingRequiredArgument):
             await ctx.send("なんのガチャ？")
+
+    @commands.command()
+    async def dice(self, ctx, num_max, num_min=1):
+        """好きな多面体と数字のサイコロふれます"""
+        await ctx.send(random.randint(num_min, num_max))
+    
+    @dice.error
+    async def dice_error(self, ctx, error):
+        if isinstance(error, commands.errors.BadArgument):
+            await ctx.send("数字で言って！")
+        if isinstance(error, commands.errors.MissingRequiredArgument):
+            await ctx.send("どんなサイコロ？")
 
 def setup(bot):
     bot.add_cog(GachaSystem(bot))
